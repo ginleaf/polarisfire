ee5ad219857f5905ad3ac607e9cd44fd32ac07dc
gin <gin@tolabaki.gr>
2022-02-24 09:08:00 -0800

    H_nuclei_density calculated, yt-based mock cube, nCR from CR specific energy dens, octree consistent with axes of sim

---
+++ process_sims/plot_data.py
@@ -0,0 +1,287 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 26 17:10:01 2021
+
+@author: gin
+"""
+
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+import os
+from matplotlib.colors import LogNorm
+from astropy.io import fits
+
+#%%
+
+# Plot polaris output
+savedir = '/home/gin/projects/FIRE/POLARIS/polarisfire/preprocess_sims/results/'
+folder = savedir+'data/'
+
+os.chdir(folder)
+
+# structure of file is :'I Q U V null Nth I Q U V lambda^2xkappa Ncr'
+
+hdu = fits.open(folder+'polaris_detector_nr0001.fits')
+dat2 = hdu[0].data[0][0]
+plt.figure(figsize=(5,6))
+plt.imshow(dat2,origin='lower', cmap = 'Blues')
+plt.colorbar(fraction=0.046, pad=0.1,orientation='horizontal')
+plt.title('Stokes I')
+plt.savefig(savedir+'plots/I_d01.png')
+plt.show()
+
+print(dat2[0][0], 'Jy/pix')
+
+# Angle of pixel
+pix_ang = hdu[0].header['CDELT1A'] #deg
+print('Pix size', pix_ang, hdu[0].header['CUNIT1A'])
+
+hdu = fits.open(folder+'polaris_detector_nr0002.fits')
+dat2 = hdu[0].data[0][0]
+plt.figure(figsize=(5,6))
+plt.imshow(dat2,origin='lower', cmap = 'Blues')
+plt.colorbar(fraction=0.046, pad=0.1,orientation='horizontal')
+plt.title('Stokes I')
+plt.savefig(savedir+'plots/I_d02.png')
+plt.show()
+
+print(dat2[0][0], 'Jy/pix')
+
+hdu = fits.open(folder+'polaris_detector_nr0003.fits')
+dat2 = hdu[0].data[0][0]
+plt.figure(figsize=(5,6))
+plt.imshow(dat2,origin='lower', cmap = 'Blues')
+plt.colorbar(fraction=0.046, pad=0.1,orientation='horizontal')
+plt.title('Stokes I')
+plt.savefig(savedir+'plots/I_d03.png')
+plt.show()
+
+print(dat2[0][0], 'Jy/pix')
+
+hdu = fits.open(folder+'polaris_detector_nr0004.fits')
+dat2 = hdu[0].data[0][0]
+plt.figure(figsize=(5,6))
+plt.imshow(dat2,origin='lower', cmap = 'Blues')
+plt.colorbar(fraction=0.046, pad=0.1,orientation='horizontal')
+plt.title('Stokes I')
+plt.savefig(savedir+'plots/I_d04.png')
+plt.show()
+
+print(dat2[0][0], 'Jy/pix')
+
+print('-----Stokes QU------')
+
+print('1st detector')
+# QU
+hdu = fits.open(folder+'polaris_detector_nr0001.fits')
+dat2 = hdu[0].data[2][0]
+plt.figure(figsize=(5,6))
+plt.imshow(dat2,origin='lower', cmap = 'Blues')
+if dat2.mean()!=0:
+    plt.colorbar(fraction=0.046, pad=0.1,orientation='horizontal')
+plt.title('U')
+plt.savefig(savedir+'plots/U_d01.png')
+plt.show()
+print('U', dat2.min(), dat2.max(), 'Jy/pix')
+dat2 = hdu[0].data[1][0]
+plt.figure(figsize=(5,6))
+plt.imshow(dat2,origin='lower', cmap = 'Blues')
+if dat2.mean()!=0:
+    plt.colorbar(fraction=0.046, pad=0.1,orientation='horizontal')
+plt.title('Q')
+plt.savefig(savedir+'plots/Q_d01.png')
+plt.show()
+print('Q', dat2.min(), np.max(dat2), 'Jy/pix')
+
+print('2nd detector')
+hdu = fits.open(folder+'polaris_detector_nr0002.fits')
+dat2 = hdu[0].data[2][0]
+plt.figure(figsize=(5,6))
+plt.imshow(dat2,origin='lower', cmap = 'Blues')
+if dat2.mean()!=0:
+    plt.colorbar(fraction=0.046, pad=0.1,orientation='horizontal')
+plt.title('U')
+plt.savefig(savedir+'plots/U_d02.png')
+plt.show()
+print('U', dat2.min(), dat2.max(), 'Jy/pix')
+dat2 = hdu[0].data[1][0]
+plt.figure(figsize=(5,6))
+plt.imshow(dat2,origin='lower', cmap = 'Blues')
+if dat2.mean()!=0:
+    plt.colorbar(fraction=0.046, pad=0.1,orientation='horizontal')
+plt.title('Q')
+plt.savefig(savedir+'plots/Q_d02.png')
+plt.show()
+print('Q', dat2.min(), np.max(dat2), 'Jy/pix')
+
+
+print('3rd detector')
+hdu = fits.open(folder+'polaris_detector_nr0003.fits')
+dat2 = hdu[0].data[2][0]
+plt.figure(figsize=(5,6))
+plt.imshow(dat2,origin='lower', cmap = 'Blues')
+if dat2.mean()!=0:
+    plt.colorbar(fraction=0.046, pad=0.1,orientation='horizontal')
+plt.title('U')
+plt.savefig(savedir+'plots/U_d03.png')
+plt.show()
+print('U', dat2.min(), dat2.max(), 'Jy/pix')
+dat2 = hdu[0].data[1][0]
+plt.figure(figsize=(5,6))
+plt.imshow(dat2,origin='lower', cmap = 'Blues')
+if dat2.mean()!=0:
+    plt.colorbar(fraction=0.046, pad=0.1,orientation='horizontal')
+plt.title('Q')
+plt.savefig(savedir+'plots/Q_d03.png')
+plt.show()
+print('Q', dat2.min(), np.max(dat2), 'Jy/pix')
+
+
+print('4rd detector')
+hdu = fits.open(folder+'polaris_detector_nr0004.fits')
+dat2 = hdu[0].data[2][0]
+plt.figure(figsize=(5,6))
+plt.imshow(dat2,origin='lower', cmap = 'Blues')
+if dat2.mean()!=0:
+    plt.colorbar(fraction=0.046, pad=0.1,orientation='horizontal')
+plt.title('U')
+plt.savefig(savedir+'plots/U_d04.png')
+plt.show()
+print('U', dat2.min(), dat2.max(), 'Jy/pix')
+dat2 = hdu[0].data[1][0]
+plt.figure(figsize=(5,6))
+plt.imshow(dat2,origin='lower', cmap = 'Blues')
+if dat2.mean()!=0:
+    plt.colorbar(fraction=0.046, pad=0.1,orientation='horizontal')
+plt.title('Q')
+plt.savefig(savedir+'plots/Q_d04.png')
+plt.show()
+print('Q', dat2.min(), np.max(dat2), 'Jy/pix')
+
+wavelength =  hdu[0].header['HIERARCH WAVELENGTH1']
+frequency = 2.99e8/wavelength/1e6#m/s/m -> MHz
+print('Wavelength (m)', wavelength, 'frequency (MHz)', frequency)
+
+#%%
+
+
+hdu = fits.open(folder+'polaris_detector_nr0004_xy.fits')
+dat2 = hdu[0].data[0][0]
+plt.figure(figsize=(5,6))
+plt.imshow(dat2,origin='lower', cmap = 'Blues')
+plt.colorbar(fraction=0.046, pad=0.1,orientation='horizontal')
+plt.title('Stokes I')
+plt.savefig(savedir+'plots/I_d04_xy.png')
+plt.show()
+
+
+
+hdu = fits.open(folder+'polaris_detector_nr0004_xy.fits')
+dat2 = hdu[0].data[2][0]
+plt.figure(figsize=(5,6))
+plt.imshow(dat2,origin='lower', cmap = 'Blues')
+if dat2.mean()!=0:
+    plt.colorbar(fraction=0.046, pad=0.1,orientation='horizontal')
+plt.title('U')
+plt.savefig(savedir+'plots/U_d04_xy.png')
+plt.show()
+print('U', dat2.min(), dat2.max(), 'Jy/pix')
+dat2 = hdu[0].data[1][0]
+plt.figure(figsize=(5,6))
+plt.imshow(dat2,origin='lower', cmap = 'Blues')
+if dat2.mean()!=0:
+    plt.colorbar(fraction=0.046, pad=0.1,orientation='horizontal')
+plt.title('Q')
+plt.savefig(savedir+'plots/Q_d04_xy.png')
+plt.show()
+
+
+
+#%%
+
+# Plot the k_V l^2 quantity
+
+hdu = fits.open(folder+'polaris_detector_nr0002.fits')
+dat2 = hdu[0].data[10][0]
+plt.figure(figsize=(5,6))
+plt.imshow(dat2,origin='lower', cmap = 'Blues')
+if dat2.mean()!=0:
+    plt.colorbar(fraction=0.046, pad=0.1,orientation='horizontal')
+plt.title(r'$\kappa_V \, \lambda^2$')
+plt.savefig(savedir+'plots/kappal2_d02.png')
+plt.show()
+
+
+hdu = fits.open(folder+'polaris_detector_nr0003.fits')
+dat2 = hdu[0].data[10][0]
+plt.figure(figsize=(5,6))
+plt.imshow(dat2,origin='lower', cmap = 'Blues')
+if dat2.mean()!=0:
+    plt.colorbar(fraction=0.046, pad=0.1,orientation='horizontal')
+plt.title(r'$\kappa_V \, \lambda^2$')
+plt.savefig(savedir+'plots/kappal2_d03.png')
+plt.show()
+
+# Q, U with Faraday rotation
+
+hdu = fits.open(folder+'polaris_detector_nr0002.fits')
+dat2 = hdu[0].data[7][0]
+plt.figure(figsize=(5,6))
+plt.imshow(dat2,origin='lower', cmap = 'Blues')
+if dat2.mean()!=0:
+    plt.colorbar(fraction=0.046, pad=0.1,orientation='horizontal')
+plt.title(r'Q')
+plt.savefig(savedir+'plots/Q_fr_d02.png')
+plt.show()
+
+
+hdu = fits.open(folder+'polaris_detector_nr0002.fits')
+dat2 = hdu[0].data[8][0]
+plt.figure(figsize=(5,6))
+plt.imshow(dat2,origin='lower', cmap = 'Blues')
+if dat2.mean()!=0:
+    plt.colorbar(fraction=0.046, pad=0.1,orientation='horizontal')
+plt.title(r'U')
+plt.savefig(savedir+'plots/U_fr_d02.png')
+plt.show()
+
+
+#%%
+# I
+
+
+c = 2.99e10
+
+k_B = 1.38e-16 #erg/K
+
+pc_to_cm = 3.08e18 #cm
+
+def I_to_K(I,wavelength):
+    wavelength_cm = wavelength*100
+    # convert I from Jy to cgs
+    return I*1e-23/(2*k_B)*wavelength_cm**2
+#%%
+hdu = fits.open(folder+'polaris_detector_nr0001.fits')
+dat2 = hdu[0].data[0][0]
+
+print np.unique(dat2)
+print np.unique(I_to_K(dat2.flatten(),wavelength)), 'K'
+
+
+hdu = fits.open(folder+'polaris_detector_nr0002.fits')
+dat2 = hdu[0].data[0][0]
+
+print np.unique(dat2)
+print np.unique(I_to_K(dat2.flatten(),wavelength)), 'K'
+
+hdu = fits.open(folder+'polaris_detector_nr0003.fits')
+dat2 = hdu[0].data[0][0]
+
+print np.unique(dat2)
+print np.unique(I_to_K(dat2.flatten(),wavelength)), 'K'
+#%%
+
+datNcr = hdu[0].data[11][0]
+print np.unique(datNcr)
---
+++ process_sims/run_polaris.py
@@ -0,0 +1,59 @@
+import subprocess
+import os
+import shutil
+
+
+def write_cmd(cmd_savename, octree_filename, results_dir, r,Npix):
+    fop=open(cmd_savename,'w')
+    fop.write('<task> 1\n')
+    fop.write('  <cmd>	CMD_SYNCHROTRON	\n  \n <max_subpixel_lvl> 0\n    ')
+    fop.write(' <path_grid>      "'+octree_filename+'"\n')
+    fop.write(' <path_out>       "./results/'+results_dir+'"\n')
+    fop.write(' #convesion into SI\n  <conv_dens>     1e6 #cm^-3 in m^-3	\n <mu>		      1.4\n')
+    fop.write(' # 1pc = 3.0857e+16 m\n')
+    fop.write(' # this is half the side length so r * 1000* 3.0857e+16\n')
+    side_length_kpc = 3.0857e+16 * r * 1000
+    fop.write(' <conv_len>      {:e} # the code assumes this is +- the side length of XX kpc\n'.format(side_length_kpc))
+    fop.write(' <conv_mag>      1e-4 # G in T\n')
+    fop.write(' <nr_threads>  -1\n  #rotation axis 100 is x axis, 001 is z axis, 010 is y axis (manual has typo)\n')
+    fop.write(' <axis1> 1 0 0\n  <axis2> 0 0 1\n')
+    fop.write(' #detectors at lambda m rotated arround each of the ax1 and ax2  axis\n')
+    fop.write(' # detector number of pixels lamda_min lambda_max number_of_wavelenghts no_bgr_source=1 angle_axis1 angle_axis2\n')
+    fop.write(' # for healpix last 3 are: r_x, r_y, r_z in meters\n')
+    fop.write(' <detector_sync nr_pixel = "{:d}">	29.9	29.9	1	1	0.0	0.0	{:e}\n'.format(Npix,side_length_kpc*10))
+    fop.write(' <detector_sync nr_pixel = "{:d}">	29.9	29.9	1	1	0.0	90.0	{:e}\n'.format(Npix,side_length_kpc*10))
+    fop.write(' <detector_sync nr_pixel = "{:d}">	29.9	29.9	1	1	90.0	0.0	{:e}\n'.format(Npix,side_length_kpc*10))
+    fop.write(' <detector_sync nr_pixel = "{:d}">	29.9	29.9	1	1	90.0	90.0	{:e}\n'.format(Npix,side_length_kpc*10))
+    fop.write('</task>\n')
+    fop.close()
+    return
+
+Npix = 32 # the number of pixels of any axis of the cube
+r = 10 # the radius (half sidelength) of the cube in kpc
+
+cmd_savename = 'cmd_file'
+octree_filename = "../../m12i_cr700/snapshot_600.0.hdf5_cut{:d}_r{:d}.dat".format(Npix,r)
+results_dir = "./results/m12i_cr700_cut{:d}_r{:d}/".format(Npix,r)
+
+# Edit cmd file
+write_cmd(cmd_savename, octree_filename, results_dir, r,Npix)
+
+# copy cmd file to results directory
+shutil.copyfile(cmd_savename, results_dir+cmd_savename)
+
+# Call polaris
+PATH_TO_POLARIS = '/home/panopg/Polaris/bin/polaris'
+
+#subprocess.call(['sh',PATH_TO_POLARIS+' '+cmd_savename])
+#output_stream = os.popen(PATH_TO_POLARIS+' '+cmd_savename)
+#output_stream.read()
+
+def run_command(command):
+    p = subprocess.Popen(command,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
+    return iter(p.stdout.readline, b'')
+
+command = [PATH_TO_POLARIS,cmd_savename]
+for line in run_command(command):
+    print(line)
